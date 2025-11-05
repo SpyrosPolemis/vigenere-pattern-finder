29015781257b156d189654cd8bc5190c3793dec8
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2025-11-05 16:28:02 +1000

    Initial commit

---
+++ ciphertextSplitter.js
@@ -0,0 +1,20 @@
+
+const keyLength = 9;
+
+const caesarCryptograms = [];
+const keyLetters = "";
+const keyNumbers = "";
+
+// for key length, 
+//  create that many arays and put them in caesarCryprograms
+// for cryptogram.length
+//  push letter i onto caesarCryptograms[something]
+// print caesar cryptograms
+
+
+// for each cryptogram
+//   count letters
+//   key = most common letter - e
+//   letter = alphabet[key]
+//  keyNMumber.append(key)
+//   key.append(letter)
