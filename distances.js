9c1a299850a790a19dd341fc48ed9bc28963117d
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2025-11-05 18:27:44 +1000

    Fix bug that was not updating ciphertext to remove patterns after each check

--- distances.js
+++ distances.js
@@ -1,18 +1,18 @@
-const ciphertext = "xkvdxvokmlavgkigbcdlfkgscvckyiewixqecbzoytxcukbrwtwuvggykhxquavfhkaupkuqfticawunvzxvizlzmgfmycxrzctqavilvkugeuvrxhlwtzbjmbpobcjrgrhakausguhbwxwnwbetketahgkdigkvzwklzabrzwcetaokiazqhxrniscmtmguekloanwtuekwnqrhklslmgmzhtedxhobulxirbxuzalaionknecjlrnwwzzclxxivhoxngfrcretlhjxuekmisdwvrmxvkvqogvdysxpkkfzlbxgzaageooezqqflyetpywhglkekijiulkfnzwkvuwhwdbwiqrdbeetrjmhxbtixrigyamyigloacjfpigejlklbfnmsnquebcimexgcuazeoismpllwrxhkzkudnalevivjheoyxnmcjmjaghgvgfezgaxkvgvwvsiszqohevmxrzqpykvfhvsavztkiftxwxwwkhxilnkubvnvcunigovrgqkvvsgupksswvwwieemmqqmlkoeixipuxyilguuoamdegxzwgvntammuvemekukigvflavakxymcjgvdamsitwilttxowpslfnxslbjwffsmtxwijxjsbzkzwdxishjnqulbdeyvklgjbtkmlkotwtkseimieqbjmtvqmftryilpkifwkjhbtovdsmklxetljalmilmuvhgkrmhhkzpwywivmkvvkmrtx"
+let ciphertext = "xkvdxvokmlavgkigbcdlfkgscvckyiewixqecbzoytxcukbrwtwuvggykhxquavfhkaupkuqfticawunvzxvizlzmgfmycxrzctqavilvkugeuvrxhlwtzbjmbpobcjrgrhakausguhbwxwnwbetketahgkdigkvzwklzabrzwcetaokiazqhxrniscmtmguekloanwtuekwnqrhklslmgmzhtedxhobulxirbxuzalaionknecjlrnwwzzclxxivhoxngfrcretlhjxuekmisdwvrmxvkvqogvdysxpkkfzlbxgzaageooezqqflyetpywhglkekijiulkfnzwkvuwhwdbwiqrdbeetrjmhxbtixrigyamyigloacjfpigejlklbfnmsnquebcimexgcuazeoismpllwrxhkzkudnalevivjheoyxnmcjmjaghgvgfezgaxkvgvwvsiszqohevmxrzqpykvfhvsavztkiftxwxwwkhxilnkubvnvcunigovrgqkvvsgupksswvwwieemmqqmlkoeixipuxyilguuoamdegxzwgvntammuvemekukigvflavakxymcjgvdamsitwilttxowpslfnxslbjwffsmtxwijxjsbzkzwdxishjnqulbdeyvklgjbtkmlkotwtkseimieqbjmtvqmftryilpkifwkjhbtovdsmklxetljalmilmuvhgkrmhhkzpwywivmkvvkmrtx"
 
-let patterns = ["", "", "", "",]
-let locations = []
+let patterns = ["et", "ig", "kv", "uek", "mlko"];
+let locations = [];
 
 for (let i = 0; i < patterns.length; i++) {
     locations.push([])
     // for each pattern, go through the text,
-    // while (ciphertext.indexOf(patterns[i]) != -1) {
-    //     // while there is pattern in cipher text
-    //     patternIndex = ciphertext.indexOf(patterns[i]) // find the index of the first pattern in remaining text 
-    //     ciphertext.replace(patterns[i], replacePattern(patterns[i])) // replace the first pattern with 1's 
-    //     locations[i].push(patternIndex) // push the position into locations array
-    //     console.log(i)
-    // }
+    while (ciphertext.indexOf(patterns[i]) != -1) {
+        // while there is pattern in cipher text
+        patternIndex = ciphertext.indexOf(patterns[i]) // find the index of the first pattern in remaining text 
+        ciphertext = ciphertext.replace(patterns[i], replacePattern(patterns[i])) // replace the first pattern with 1's 
+        locations[i].push(patternIndex) // push the position into locations array
+        console.log(patternIndex + ciphertext)
+    }
 }
 
 function replacePattern(pattern) {
@@ -21,11 +21,7 @@ function replacePattern(pattern) {
     for (let j = 0; j < length; j++) {
         replacementString += "1";
     }
-    console.log(replacementString)
     return replacementString;
 }
 
 console.log("testing distances")
-
-
-// SPLIT THE ARRAY ON PATTERN, RETURN THE LEFT SIDE AND GET COUNT->WHICH IS POSITION
